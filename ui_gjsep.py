a9f45c4f2f9360d9e78da07f12525eb860220695
Marc Ducobu <marc@champs-libres.coop>
2015-02-05 14:45:40 +0100

    GPVv3 -> GPLv2

--- ui_gjsep.py
+++ ui_gjsep.py
@@ -9,7 +9,7 @@
 
     QGISGeoJSONExportPlugin is free software: you can redistribute it and/or
     modify it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
+    the Free Software Foundation, either version 2 of the License, or
     (at your option) any later version.
 
     QGISGeoJSONExportPlugin is distributed in the hope that it will be useful,
@@ -20,7 +20,7 @@
     You should have received a copy of the GNU General Public License along
     with QGISGeoJSONExportPlugin.  If not, see <http://www.gnu.org/licenses/>.
 """
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-3
 
 from PyQt4 import QtCore, QtGui
 import shelve
