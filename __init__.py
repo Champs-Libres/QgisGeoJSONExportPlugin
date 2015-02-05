9cb7ddebbc98b1f9da23d428d8387fed26b5fc33
Marc Ducobu <marc@champs-libres.coop>
2015-02-05 14:21:02 +0100

    Adding License

--- __init__.py
+++ __init__.py
@@ -1,3 +1,26 @@
+"""
+    QGISGeoJSONExportPlugin is QGIS plugin that helps users to convert
+    vectorial layer into a GeoJSON file and sends it to a remote server via
+    ftp, sftp or ftps.
+
+    Copyright (C) 2015 Champs-Libres Cooperative <info@Champs-libres.coop>
+
+    This file is part of QGISGeoJSONExportPlugin.
+
+    QGISGeoJSONExportPlugin is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    QGISGeoJSONExportPlugin is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with QGISGeoJSONExportPlugin.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 def classFactory(iface):
     from gjsep import GeoJSONExportPlugin
     return GeoJSONExportPlugin(iface)
--- ui_gjsep.py
+++ ui_gjsep.py
@@ -1,11 +1,26 @@
-# -*- coding: utf-8 -*-
+"""
+    QGISGeoJSONExportPlugin is QGIS plugin that helps users to convert
+    vectorial layer into a GeoJSON file and sends it to a remote server via
+    ftp, sftp or ftps.
 
-# Form implementation generated from reading ui file 'ui_mainPlugin.ui'
-#
-# Created: Mon Dec 29 14:04:36 2014
-#      by: PyQt4 UI code generator 4.11.1
-#
-# WARNING! All changes made in this file will be lost!
+    Copyright (C) 2015 Champs-Libres Cooperative <info@Champs-libres.coop>
+
+    This file is part of QGISGeoJSONExportPlugin.
+
+    QGISGeoJSONExportPlugin is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    QGISGeoJSONExportPlugin is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with QGISGeoJSONExportPlugin.  If not, see <http://www.gnu.org/licenses/>.
+"""
+# -*- coding: utf-8 -*-
 
 from PyQt4 import QtCore, QtGui
 import shelve
